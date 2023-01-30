64b4a46b80feee752211f74ea41ac0cb90f56d1d
Choiyounho <maxosa78@gmail.com>
2023-01-31 01:21:32 +0900

    feat : 컬러 추가

--- app/src/main/java/linecraft/plavel/ui/theme/Color.kt
+++ app/src/main/java/linecraft/plavel/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val SelectBottomNavigationItemColor = Color(0xFFFFFFFF)
+val UNSelectBottomNavigationItemColor = Color(0xFFA9B2B9)
+
+val MainBackgroundColor = Color(0xFF212428)
