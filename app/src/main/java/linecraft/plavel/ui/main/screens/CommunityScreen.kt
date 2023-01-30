5dabd6b9745111fdbb60ae0e1c5e609831a25bf7
Choiyounho <maxosa78@gmail.com>
2023-01-31 01:24:27 +0900

    feat : 커뮤니티 페이지 기본 셋팅

---
+++ app/src/main/java/linecraft/plavel/ui/main/screens/CommunityScreen.kt
@@ -0,0 +1,26 @@
+package linecraft.plavel.ui.main.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import linecraft.plavel.R
+
+@Composable
+fun CommunityScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = stringResource(id = R.string.community),
+            textAlign = TextAlign.Center,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+}
